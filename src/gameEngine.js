0cd2960f28ac83efd6ab72ac07699b244fd563a8
Nadeen Hussein <nadeen.hussein1@ibm.com>
2022-01-09 23:20:26 +0100

    test: Given cell in board when I want check neighbors then return list neighbors pass

---
+++ src/gameEngine.js
@@ -0,0 +1,40 @@
+class GameEngine {
+  constructor(board) {
+    this.board = board;
+  }
+
+  getNeighbors(x, y) {
+    const neighborsArray = [];
+    for (let i = -1; i < 2; i++) {
+      for (let j = -1; j < 2; j++) {
+        const isSameCell = i === 0 && j === 0;
+        if (!isSameCell && this.isInsideBoard(x + i, y + j)) {
+          neighborsArray.push(this.board[x + i][y + j]);
+        }
+      }
+    }
+
+    return neighborsArray;
+  }
+
+  getNeighborsBombCount(cell) {
+    const neighbors = this.getNeighbors(cell.x, cell.y);
+    let count = 0;
+    for (let i in neighbors) {
+      if (neighbors[i].isBomb()) count++;
+    }
+
+    return count;
+  }
+
+  areAllNotNeighborsBombs(neighborsArray) {
+    return neighborsArray.every((neighbor) => !neighbor.isBomb());
+  }
+
+  isInsideBoard(x, y) {
+    const boardSize = this.board.length;
+    return x >= 0 && x < boardSize && y >= 0 && y < boardSize;
+  }
+}
+
+module.exports = { GameEngine };
