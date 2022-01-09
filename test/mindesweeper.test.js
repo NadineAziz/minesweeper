909b12a4aa889c8e4193785a1b318be2d184c96c
Nadeen Hussein <nadeen.hussein1@ibm.com>
2022-01-09 22:08:04 +0100

    test: Given a new board when displayed then I want to get an empty board fail

--- test/mindesweeper.test.js
+++ test/mindesweeper.test.js
@@ -1,5 +1,14 @@
 describe('MineSweeper', () => {
-  it('First test foo', () => {
-    expect(1).toBe(1);
+  describe('User Story 1: As a player,I want to see different outputs displayed so that I can play', () => {
+    it('Given a new board when displayed then I want to get an empty board', () => {
+      const board = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0],
+      ];
+      expect(board).toBe(
+        '+-+-+-+-+-+-+\n|   |   |   | \n+-+-+-+-+-+-+\n|   |   |   | \n+-+-+-+-+-+-+\n|   |   |   | \n+-+-+-+-+-+-+\n'
+      );
+    });
   });
 });
