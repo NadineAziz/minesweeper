9c3f01d2037cb5e76992d15b680586d4ed169274
Nadeen Hussein <nadeen.hussein1@ibm.com>
2022-01-09 23:30:41 +0100

    test: Given a cell in the board when I want check neighbors are not bombs then pass pass

--- src/gameEngine.js
+++ src/gameEngine.js
@@ -31,6 +31,9 @@ class GameEngine {
     const boardSize = this.board.length;
     return x >= 0 && x < boardSize && y >= 0 && y < boardSize;
   }
+  areAllNotNeighborsBombs(neighborsArray) {
+    return neighborsArray.every((neighbor) => !neighbor.isBomb());
+  }
 }
 
 module.exports = { GameEngine };
